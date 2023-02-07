44a1ba1c2cfe1d4880a18847cd476ef3cd16d543
Stanislaw Malinowski <s.malinowski197@wp.pl>
2023-02-07 18:04:58 +0100

    First commit

---
+++ src/main/java/com/kodilla/KodillaCourse1Application.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourse1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourse1Application.class, args);
+    }
+
+}
